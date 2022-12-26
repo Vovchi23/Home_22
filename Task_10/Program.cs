777b859345d04ee568e4422ae6a31c6898453d2d
Vivchik <vovka.azik@mail.ru>
2022-12-26 17:53:10 +0300

    Task_10

---
+++ Task_10/Program.cs
@@ -0,0 +1,15 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное число
+// на выходе показывает вторую цифру этого числа.
+
+Console.Clear();
+
+Console.Write("Введите трех значное число ");
+
+int number = int.Parse(Console.ReadLine()!);
+ if (number <0)
+ {
+    number = number*(-1);
+ }
+ Console.WriteLine(number);
+int digi = number/10;
+Console.WriteLine(digi%10);
